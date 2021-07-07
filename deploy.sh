0c54f495afa870639ba6be8964bbc371b669c4aa
Shan Yu Lin <ineslin35@gmail.com>
2021-07-07 22:21:03 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:12cassie34/self-introduction.git main:gh-pages
 
